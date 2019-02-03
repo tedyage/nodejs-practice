a540c1591b2f34e3a764f0df7a1746d1e9f1f507
tedyage <tedyage@sina.com>
2019-02-03 22:00:21 +0800

    add config.js

---
+++ lesson08-sequelize/config.js
@@ -0,0 +1,9 @@
+var config={
+    database:"practice",     //数据库
+    username:"root",         //用户名
+    password:"a1b2c3d4",     //密码
+    host:"localhost",        //主机名
+    port:3306                //端口号
+};
+
+module.exports = config;
