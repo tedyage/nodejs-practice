84fcc80f550654751b17857b709bdc15fec7eb57
tedyage <tedyage@sina.com>
2019-02-04 02:03:46 +0800

    extends the sequelize instanse

--- lesson08-sequelize/app.js
+++ lesson08-sequelize/app.js
@@ -32,8 +32,8 @@ var Pet = sequelize.define('pet',{
 var addPet = async() =>{
     var dog = await Pet.create({
         id: 'd-'+Date.now(),
-        name:'Odie',
-        gender:false,
+        name:'Teddy',
+        gender:true,
         birth:new Date().getFullYear()+"-"+(new Date().getMonth()+1)+"-"+new Date().getDate(),
         createdAt:Date.now(),
         updatedAt:Date.now(),
@@ -50,10 +50,90 @@ var getPet = async(name)=> {
         }
     });
     console.log(`find ${pets.length} pets:` );
-    for(var p in pets){
-        console.log(JSON.stringify(pets));
+    for(var p of pets){
+        console.log(JSON.stringify(p));
     }
     return pets.length>0?pets[0]:null;
+};
+
+//查询Pet的name和Gender
+var getPetNameAndGender = async()=>{
+    var result = await Pet.findAll({
+        attributes:["name","gender"]
+    });
+    if(result){
+        console.log(`find ${result.length} pets:`);
+        for(var p of result){
+            console.log(JSON.stringify(p));
+        }
+    }   
+};
+
+//查询Pet的创建时间，列名为CreateTime
+var getCreateTimeByName = async(name)=>{
+    var result = await Pet.findAll({
+        attributes:["name","gender",['createdAt','createtime']],
+        where:{
+            name:name
+        }
+    });
+    if(result){
+        console.log(`find ${result.length} pets:`);
+        for(var p of result){
+            console.log(JSON.stringify(p));
+        }
+    }   
+};
+
+//查询宠物性别的数量
+var getCountByGender = async()=>{
+    var result = await Pet.findAll({
+        attributes:[[sequelize.fn('COUNT',sequelize.col('gender')),'sex']]
+    });
+    if(result){
+        console.log(`The gender count is ${result[0].get('sex')}`);
+    } 
+};
+
+//根据name和gender条件的交集做条件查询Pet
+var getPetByNameAndGender = async(name,gender)=>{
+    var pet = await Pet.find({
+        where:{
+            name:name,
+            gender:gender
+        }
+    });
+    if(pet){
+        console.log(`The pet is ${JSON.stringify(pet)}`);
+    }
+}
+
+//根据name or gender的值查询Pet
+var getPetByNameOrGender = async(name,gender)=>{
+    var pets = await Pet.findAll({
+        where:{
+            [sequelize.Op.or]:[{name:name},{gender:gender}]
+        }
+    });
+    if(pets){
+        console.log(`The pets count is ${pets.length}.`);
+        for(var p of pets){
+            console.log(JSON.stringify(p));
+        }
+    }
+};
+
+//根据createdAt倒叙排序查询Pets
+var getPetsOrderByCreatedAtDesc = async()=>{
+    var pets = await Pet.findAll({
+        order:[['createdAt','DESC']]
+    });
+    if(pets){
+        console.log(`The pets count is ${pets.length}.`);
+        for(var p of pets){
+            console.log(JSON.stringify(p));
+        }
+    }
 };
 
 //编辑并保存查询出的Pet实例，完成数据更新
@@ -75,9 +155,17 @@ var destoryPet = async(name)=>{
     }
 };
 
-addPet();
-getPet("Odie");
-updatePet('Odie');
-getPet("Odie");
-destoryPet('Odie');
-getPet("Odie");
+//addPet();
+//getPet("Odie");
+//updatePet('Odie');
+//getPet("Odie");
+//destoryPet('Odie');
+//getPet("Odie");
+
+getPet('Odie');
+getPetNameAndGender();
+getCreateTimeByName('Teddy');
+getCountByGender();
+getPetByNameAndGender('Teddy',true);
+getPetByNameOrGender('Odie2',true);
+getPetsOrderByCreatedAtDesc();
